879ca430e52a03db838e1fe9dbdf96b7ceced2a9
Lucas Figueira <lucasfigueira137@gmail.com>
2026-02-17 12:20:45 -0300

    added store/adminStore.ts

---
+++ src/store/adminStore.ts
@@ -0,0 +1,20 @@
+import { AdminStore } from "@/types/store/adminStore";
+import { LeadType } from "@/types/coach";
+import { SignalVersion, Tool } from "@/views/Admin/types";
+import { create } from "zustand";
+
+export const useAdminStore = create<AdminStore>((set, get) => ({
+    selectedTool: 'cue-signals',
+    leadType: 'buyer',
+
+    signalSheets: [],
+    activeSheetVersion: 1,
+    liveSheetVersion: 1,
+
+    setSelectedTool: (selectedTool: Tool) => set({ selectedTool }),
+    setLeadType: (leadType: LeadType) => set({ leadType }),
+
+    setSignalSheets: (sheets: SignalVersion[]) => set({ signalSheets: sheets }),
+    setActiveSheetVersion: (version: number) => set({ activeSheetVersion: version }),
+    setLiveSheetVersion: (version: number) => set({ liveSheetVersion: version }),
+}));
